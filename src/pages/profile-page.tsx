e43f5ea2fa8f26e9cf122a75805811d30bb0196c
shrishan9669 <shrishan9669@gmail.com>
2025-04-10 10:19:04 +0530

    Update profile-page.tsx

--- src/pages/profile-page.tsx
+++ src/pages/profile-page.tsx
@@ -166,11 +166,12 @@ export default function Profilepage() {
 
                 <button
                     onClick={() => {
-                        localStorage.removeItem('number');
-                        localStorage.removeItem('token');
-                        localStorage.removeItem('role');
-                        localStorage.removeItem('class');
-                        localStorage.removeItem('userid');
+                       localStorage.removeItem('number')
+                        localStorage.removeItem('token')
+                        localStorage.removeItem('userid')
+                        localStorage.removeItem('promo')
+                        localStorage.removeItem('role')
+                        localStorage.removeItem('class')
                         window.location.href = '/';
                     }}
                     className="px-4 py-2 bg-gray-800 text-white rounded-full cursor-pointer font-medium hover:bg-gray-700 transition-colors duration-200 self-center sm:self-auto"
